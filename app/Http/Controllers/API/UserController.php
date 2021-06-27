df937f7f409d759c4f00623583c7bc205828abf0
Sami Younas <samiys005@gmail.com>
2021-06-27 17:17:18 +0500

    Register and Login working

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers\API;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+class UserController extends Controller
+{
+    public $successStatus = 200;
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request){
+
+        $data = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect Details.
+            Please try again'], 401);
+        }
+        $token =  Auth::user()->createToken('MyApp')-> accessToken;
+        return response(['user' => auth()->user(), 'token' => $token]);
+    }
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response(['error'=>$validator->errors()], 401);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $token =  $user->createToken('MyApp')->accessToken;
+        return response([ 'user' => $user, 'token' => $token]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'App\Http\Controllers\API\UserController@login');
+Route::post('register', 'App\Http\Controllers\API\UserController@register');
